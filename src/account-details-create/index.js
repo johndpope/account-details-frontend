7fd06c1af5dbbb6b096ebc3ce48c925299824b91
Steve Pole <steve.pole@transferwise.com>
2018-10-12 15:27:48 +0100

    Include downstream dependencies of account create form

--- src/account-details-create/index.js
+++ src/account-details-create/index.js
@@ -1,11 +1,10 @@
 import angular from 'angular';
 import AccountDetailsCreate from './account-details-create.component';
 import AccountDetailsService from '../account-details-service/';
-// import { fieldset } from 'styleguide-components';
 
 export default angular
   .module('tw.account-details.create', [
-    AccountDetailsService
-    // ,'tw.styleguide-components'
+    AccountDetailsService,
+    'tw.styleguide-components'
   ])
   .component('accountDetailsCreate', AccountDetailsCreate).name;
