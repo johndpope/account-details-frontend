c8d64de6c2c35ef518a61ea7216a7535f2fedcba
Steve Pole <steve.pole@transferwise.com>
2018-11-27 14:07:18 +0000

    Fix tests for account details service

--- src/account-details-service/account-details.spec.js
+++ src/account-details-service/account-details.spec.js
@@ -14,26 +14,34 @@ describe('Given a service for interacting with the acount details API', function
     $httpBackend = $injector.get('$httpBackend');
   }));
 
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
   describe('when requesting account requirements', function() {
     describe('with a currency', function() {
       var promise;
       beforeEach(function() {
-        $httpBackend.whenGET('/account-requirements?target=GBP').respond(['first']);
+        $httpBackend.whenGET('/account-requirements?target=GBP').respond(200, ['first']);
         spyOn(AccountDetailsLegacyService, 'prepareResponse').and.returnValue(['second']);
 
         promise = service.getRequirements('GBP');
-        $httpBackend.flush();
       });
 
       it('should make a GET call to the API', function() {
         $httpBackend.expectGET('/account-requirements?target=GBP');
+        $httpBackend.flush();
       });
       it('should use the legacy service to prepare the API response', function() {
+        $httpBackend.flush();
         expect(AccountDetailsLegacyService.prepareResponse).toHaveBeenCalledWith('GBP', ['first']);
       });
       it('should return a promise with the prepared response', function() {
+
+        $httpBackend.flush();
         promise.then(function(response) {
-          expect(response.data).toBe(['second']);
+          expect(response.data).toEqual(['second']);
         });
       });
     });
@@ -47,22 +55,24 @@ describe('Given a service for interacting with the acount details API', function
     describe('with a quote', function() {
       var promise;
       beforeEach(function() {
-        $httpBackend.whenGET('/quotes/123/account-requirements').respond(['first']);
+        $httpBackend.whenGET('/quotes/123/account-requirements').respond(200, ['first']);
         spyOn(AccountDetailsLegacyService, 'prepareResponse').and.returnValue(['second']);
 
         promise = service.getRequirementsForQuote(123, 'GBP');
-        $httpBackend.flush();
       });
 
       it('should make a GET call to the API', function() {
         $httpBackend.expectGET('/quotes/123/account-requirements');
+        $httpBackend.flush();
       });
       it('should use the legacy service to prepare the API response', function() {
+        $httpBackend.flush();
         expect(AccountDetailsLegacyService.prepareResponse).toHaveBeenCalledWith('GBP', ['first']);
       });
       it('should return a promise with the prepared response', function() {
+        $httpBackend.flush();
         promise.then(function(response) {
-          expect(response.data).toBe(['second']);
+          expect(response.data).toEqual(['second']);
         });
       });
     });
@@ -85,24 +95,29 @@ describe('Given a service for interacting with the acount details API', function
 
         spyOn(AccountDetailsLegacyService, 'formatModelForAPI').and.returnValue(formattedModel);
         spyOn(AccountDetailsLegacyService, 'prepareResponse').and.returnValue(['second']);
-        $httpBackend.whenPOST('/account-requirements?target=GBP').respond(['first']);
+        $httpBackend.whenPOST('/account-requirements?target=GBP').respond(200, ['first']);
 
         promise = service.refreshRequirements('GBP', model);
-        $httpBackend.flush();
       });
 
       it('should use the legacy service to format the model', function() {
+
+        $httpBackend.flush();
         expect(AccountDetailsLegacyService.formatModelForAPI).toHaveBeenCalledWith(model);
       });
       it('should POST the formatted model to the API', function() {
         $httpBackend.expectPOST('/account-requirements?target=GBP', formattedModel);
+        $httpBackend.flush();
       });
       it('should use the legacy service to prepare the response', function() {
+        $httpBackend.flush();
         expect(AccountDetailsLegacyService.prepareResponse).toHaveBeenCalledWith('GBP', ['first']);
       });
       it('should return a promise with the prepared response', function() {
+
+        $httpBackend.flush();
         promise.then(function(response) {
-          expect(response.data).toBe(['second']);
+          expect(response.data).toEqual(['second']);
         });
       });
     });
@@ -133,19 +148,22 @@ describe('Given a service for interacting with the acount details API', function
       $httpBackend.whenPOST('/accounts').respond(function() { return [401, ['errors']]});
 
       promise = service.save(model);
-      $httpBackend.flush();
     });
 
     it('should use the legacy service to format the model', function() {
+      $httpBackend.flush();
       expect(AccountDetailsLegacyService.formatModelForAPI).toHaveBeenCalledWith(model);
     });
     it('should POST the formatted model to the API', function() {
       $httpBackend.expectPOST('/accounts', formattedModel);
+      $httpBackend.flush();
     });
     it('should use the legacy service to format error responses', function() {
+      $httpBackend.flush();
       expect(AccountDetailsLegacyService.formatErrorsForDisplay).toHaveBeenCalledWith(['errors']);
     });
     it('should return a rejected promise with the formatted errors', function() {
+      $httpBackend.flush();
       promise.catch(function(errors) {
         expect(errors).toBe(['formatted']);
       });
@@ -153,6 +171,7 @@ describe('Given a service for interacting with the acount details API', function
 
     describe('without a model', function() {
       it('should throw an error', function() {
+        $httpBackend.flush();
         expect(service.save).toThrow();
       });
     });
@@ -170,21 +189,27 @@ describe('Given a service for interacting with the acount details API', function
       });
 
       promise = service.lookupAccountByEmail('test@test.com');
-      $httpBackend.flush();
     });
 
     it('should POST the formatted model to the API', function() {
-      $httpBackend.expectPOST('/accounts', model);
+      $httpBackend.expectPOST('/api/v1/uniqueId/uniqueIdLookUp', {
+        email: 'test@test.com',
+        type: "email",
+        currencyCode: "GBP"
+      });
+      $httpBackend.flush();
     });
 
     it('should return a promise with the list of matching accounts', function() {
+      $httpBackend.flush();
       promise.then(function(response) {
-        expect(response.data).toBe(['accounts']);
+        expect(response.data).toEqual(['accounts']);
       });
     });
 
     describe('without supplying an email', function() {
       it('should throw an error', function() {
+        $httpBackend.flush();
         expect(service.lookupAccountByEmail).toThrow();
       });
     });
