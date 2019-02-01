f958d7fa555463d3e1a4d1e09f5dd104ac29789f
George Stefanis <george.stefanis@transferwise.com>
2019-02-01 14:28:01 +0200

    Updates Karma and switches to Chrome headless

--- karma-conf.js
+++ karma-conf.js
@@ -1,5 +1,9 @@
 const karmaConf = require('./karma.json');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set(karmaConf);
+  config.set({
+    browsers: ['ChromeHeadless']
+  });
 };
